17852d236afcb56362b9a9840b837c195a1eed3d
ilyosbekibrohimov <ilyos199822@gmail.com>
2021-01-07 23:00:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android/navigation/NavHostFragment.kt
@@ -0,0 +1,38 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+class NavHostFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val  binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title,  container,  false)
+
+        binding.playButton.setOnClickListener {
+           view:View->view.findNavController().navigate(R.id.action_navHostFragment_to_gameFragment)
+       }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+          return NavigationUI.
+                  onNavDestinationSelected(item, requireView().findNavController())
+                  || super.onOptionsItemSelected(item)
+    }
+
+
+}
