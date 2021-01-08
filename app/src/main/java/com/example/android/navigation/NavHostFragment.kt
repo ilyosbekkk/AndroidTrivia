50f2cb9be14fae64acafbb3b3fedc0a49bad49db
ilyosbekibrohimov <ilyos199822@gmail.com>
2021-01-08 13:52:14 +0900

    drawer and app menu are now working

--- app/src/main/java/com/example/android/navigation/NavHostFragment.kt
+++ app/src/main/java/com/example/android/navigation/NavHostFragment.kt
@@ -1,6 +1,7 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
@@ -29,9 +30,8 @@ class NavHostFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-          return NavigationUI.
-                  onNavDestinationSelected(item, requireView().findNavController())
-                  || super.onOptionsItemSelected(item)
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                ||super.onOptionsItemSelected(item)
     }
 
 
